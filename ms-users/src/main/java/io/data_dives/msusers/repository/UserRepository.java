9bc0dc9a106486a7c0351b2180c2a37a6b506e18
gerson-eduardo <ishgardstorm@gmail.com>
2024-04-30 16:48:27 -0300

    feat: created the findByCpf method

--- ms-users/src/main/java/io/data_dives/msusers/repository/UserRepository.java
+++ ms-users/src/main/java/io/data_dives/msusers/repository/UserRepository.java
@@ -3,5 +3,8 @@ package io.data_dives.msusers.repository;
 import io.data_dives.msusers.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByCpf(String cpf);
 }
