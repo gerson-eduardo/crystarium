c4575be1c530bc5b191c98ca7d43cf6b2e04ebd1
gerson-eduardo <ishgardstorm@gmail.com>
2024-04-30 16:31:44 -0300

    feat: created the User entity class

---
+++ ms-users/src/main/java/io/data_dives/msusers/model/User.java
@@ -0,0 +1,25 @@
+package io.data_dives.msusers.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.ZonedDateTime;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String cpf;
+    private String email;
+    private String password;
+    private ZonedDateTime created;
+    private ZonedDateTime modified;
+}
