e55dbabdae7e382a6c20e6f550a0a3a0fc7a5050
gerson-eduardo <ishgardstorm@gmail.com>
2024-04-30 16:27:10 -0300

    feat: added a swagger description to the hello method

--- ms-users/src/main/java/io/data_dives/msusers/controller/v1/Api.java
+++ ms-users/src/main/java/io/data_dives/msusers/controller/v1/Api.java
@@ -1,5 +1,8 @@
 package io.data_dives.msusers.controller.v1;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -7,9 +10,12 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/v1")
+@Tag(name = "api")
 public class Api {
 
     @GetMapping("")
+    @Operation(summary = "Check if the system is responding", method = "GET")
+    @ApiResponse(responseCode = "200", description = "Service is online")
     public ResponseEntity<String> hello(){
         return ResponseEntity.ok("Hello");
     }
