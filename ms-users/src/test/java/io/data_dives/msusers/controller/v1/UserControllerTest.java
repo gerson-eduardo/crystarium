95e9557176679bb5e0b7bbc278ac0500f3632349
gerson-eduardo <ishgardstorm@gmail.com>
2024-05-01 15:24:07 -0300

    test: created the UserControllerTest class

---
+++ ms-users/src/test/java/io/data_dives/msusers/controller/v1/UserControllerTest.java
@@ -0,0 +1,24 @@
+package io.data_dives.msusers.controller.v1;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class UserControllerTest {
+
+    @Test
+    void createUser() {
+    }
+
+    @Test
+    void selectUserByCpf() {
+    }
+
+    @Test
+    void selectAll() {
+    }
+
+    @Test
+    void updateEmail() {
+    }
+}
