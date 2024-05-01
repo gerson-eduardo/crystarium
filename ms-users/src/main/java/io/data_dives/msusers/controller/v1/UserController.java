5eedd9ccd0be55382702b7233140a278f6504e49
gerson-eduardo <ishgardstorm@gmail.com>
2024-05-01 15:15:20 -0300

    feat: implemented the updateEmail method into the UserController

--- ms-users/src/main/java/io/data_dives/msusers/controller/v1/UserController.java
+++ ms-users/src/main/java/io/data_dives/msusers/controller/v1/UserController.java
@@ -2,6 +2,7 @@ package io.data_dives.msusers.controller.v1;
 
 import io.data_dives.msusers.dto.user.CreateUserDto;
 import io.data_dives.msusers.dto.user.SelectUserDto;
+import io.data_dives.msusers.ex.user.EmailConflictException;
 import io.data_dives.msusers.ex.user.UserConflictException;
 import io.data_dives.msusers.ex.user.UserNotFoundException;
 import io.data_dives.msusers.service.v1.UserService;
@@ -10,11 +11,14 @@ import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/api/v1")
@@ -58,5 +62,21 @@ public class UserController {
     @GetMapping("/user")
     public ResponseEntity<List<SelectUserDto>> selectAll(){
         return ResponseEntity.ok(service.selectAll());
+    }
+
+    @PutMapping("/user/email")
+    @Operation(summary = "Updates an user email")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "email updated sucessfully")
+    })
+    public ResponseEntity<String> updateEmail(@RequestBody HashMap<String, String> map){
+        try {
+            service.updateEmail(map.get("email"), map.get("cpf"));
+            return ResponseEntity.ok("Email updated sucessfully");
+        }catch (EmailConflictException e){
+            return ResponseEntity.status(409).body("Email already in use");
+        }catch (UserNotFoundException e){
+            return ResponseEntity.status(404).body("User not found");
+        }
     }
 }
