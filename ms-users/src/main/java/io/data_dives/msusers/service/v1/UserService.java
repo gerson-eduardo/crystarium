cb17d4d66a50ef5cbc835ca55e303e66ece1abcd
gerson-eduardo <ishgardstorm@gmail.com>
2024-05-01 15:03:34 -0300

    feat: added more methods to the UserService class

--- ms-users/src/main/java/io/data_dives/msusers/service/v1/UserService.java
+++ ms-users/src/main/java/io/data_dives/msusers/service/v1/UserService.java
@@ -1,14 +1,19 @@
 package io.data_dives.msusers.service.v1;
 
 import io.data_dives.msusers.dto.user.CreateUserDto;
-import io.data_dives.msusers.ex.UserConflictException;
+import io.data_dives.msusers.dto.user.SelectUserDto;
+import io.data_dives.msusers.ex.user.EmailConflictException;
+import io.data_dives.msusers.ex.user.UserConflictException;
+import io.data_dives.msusers.ex.user.UserNotFoundException;
 import io.data_dives.msusers.model.User;
 import io.data_dives.msusers.repository.UserRepository;
+import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.time.Clock;
 import java.time.ZonedDateTime;
+import java.util.List;
 
 @Service
 public class UserService {
@@ -20,6 +25,8 @@ public class UserService {
         this.repository = repository;
         this.clock = clock;
     }
+
+    @Transactional
     public void createUser(CreateUserDto dto){
         //checks if user exist
         User user = repository.findByCpf(dto.getCpf()).orElse(null);
@@ -36,6 +43,39 @@ public class UserService {
         user.setCreated(ZonedDateTime.now(clock));
         user.setModified(ZonedDateTime.now(clock));
 
+        repository.save(user);
+    }
+
+    public SelectUserDto selectByCpf(String cpf){
+        User user = repository.findByCpf(cpf).orElse(null);
+        if(user == null){
+            throw new UserNotFoundException("User not found!");
+        }
+
+        return new SelectUserDto(user);
+    }
+
+    public List<SelectUserDto> selectAll(){
+        return repository.findAll().stream()
+                .map(SelectUserDto::new)
+                .toList();
+    }
+
+    @Transactional
+    public void updateEmail(String cpf, String email){
+        User user;
+
+        user = repository.findByEmail(email).orElse(null);
+        if(user != null){
+            throw new EmailConflictException("Email already in use!");
+        }
+
+        user = repository.findByCpf(cpf).orElse(null);
+        if(user == null){
+            throw new UserNotFoundException("User not found!");
+        }
+
+        user.setEmail(email);
         repository.save(user);
     }
 }
