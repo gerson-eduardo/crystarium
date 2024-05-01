8689971568efd76d0aa17ff66824e6624ab0fc76
gerson-eduardo <ishgardstorm@gmail.com>
2024-05-01 15:05:37 -0300

    feat: created the selectUserByCpf method in the UserController

--- ms-users/src/main/java/io/data_dives/msusers/controller/v1/UserController.java
+++ ms-users/src/main/java/io/data_dives/msusers/controller/v1/UserController.java
@@ -1,19 +1,18 @@
 package io.data_dives.msusers.controller.v1;
 
 import io.data_dives.msusers.dto.user.CreateUserDto;
-import io.data_dives.msusers.ex.UserConflictException;
-import io.data_dives.msusers.model.User;
+import io.data_dives.msusers.dto.user.SelectUserDto;
+import io.data_dives.msusers.ex.user.UserConflictException;
+import io.data_dives.msusers.ex.user.UserNotFoundException;
 import io.data_dives.msusers.service.v1.UserService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/v1")
@@ -35,6 +34,20 @@ public class UserController {
             return ResponseEntity.status(201).body("User created sucessfully!");
         }catch (UserConflictException e){
             return ResponseEntity.status(409).body("User already exists");
+        }
+    }
+
+    @GetMapping("/user/cpf/{cpf}")
+    @Operation(summary = "Select user by cpf")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "User found sucessfully"),
+            @ApiResponse(responseCode = "404", description = "User not found")
+    })
+    public ResponseEntity<SelectUserDto> selectUserByCpf(@PathVariable String cpf){
+        try {
+            return ResponseEntity.ok(service.selectByCpf(cpf));
+        }catch (UserNotFoundException e){
+            return new ResponseEntity<>(HttpStatusCode.valueOf(404));
         }
     }
 }
