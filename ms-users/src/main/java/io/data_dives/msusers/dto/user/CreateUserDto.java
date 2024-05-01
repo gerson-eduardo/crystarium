f037ef114308741e4a0bf35985562481f1be82c4
gerson-eduardo <ishgardstorm@gmail.com>
2024-05-01 13:12:51 -0300

    feat: created a new constructor for the CreateUserDto to facilitate tests

--- ms-users/src/main/java/io/data_dives/msusers/dto/user/CreateUserDto.java
+++ ms-users/src/main/java/io/data_dives/msusers/dto/user/CreateUserDto.java
@@ -1,5 +1,6 @@
 package io.data_dives.msusers.dto.user;
 
+import io.data_dives.msusers.model.User;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -12,5 +13,12 @@ public class CreateUserDto {
     private String cpf;
     private String email;
     private String password;
+
+    public CreateUserDto(User user){
+        this.name = user.getName();
+        this.cpf = user.getCpf();
+        this.email = user.getEmail();
+        this.password = user.getPassword();
+    }
 }
 
