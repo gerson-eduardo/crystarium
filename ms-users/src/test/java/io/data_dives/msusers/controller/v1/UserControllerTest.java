613b39bb3919c389688c44ce8f5e612772253f98
gerson-eduardo <ishgardstorm@gmail.com>
2024-06-25 16:41:04 -0300

    test: created tests for the selectUserByCpf api call

--- ms-users/src/test/java/io/data_dives/msusers/controller/v1/UserControllerTest.java
+++ ms-users/src/test/java/io/data_dives/msusers/controller/v1/UserControllerTest.java
@@ -3,6 +3,7 @@ package io.data_dives.msusers.controller.v1;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import io.data_dives.msusers.ex.user.UserConflictException;
+import io.data_dives.msusers.ex.user.UserNotFoundException;
 import io.data_dives.msusers.service.v1.UserService;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -14,8 +15,7 @@ import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
 import static io.data_dives.msusers.test_objects.UserTestObj.*;
-import static org.mockito.Mockito.doNothing;
-import static org.mockito.Mockito.doThrow;
+import static org.mockito.Mockito.*;
 
 @WebMvcTest(UserController.class)
 class UserControllerTest {
@@ -49,7 +49,23 @@ class UserControllerTest {
     }
 
     @Test
-    void selectUserByCpf() {
+    void selectUserByCpf_user_exist() throws Exception {
+        when(service.selectByCpf(USER1.getCpf())).thenReturn(S_USER_DTO1);
+
+        mvc.perform(MockMvcRequestBuilders.get("/api/v1/user/cpf/" + USER1.getCpf())
+                .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.content().contentType(MediaType.APPLICATION_JSON))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.name").value("Estinien"));
+    }
+
+    @Test
+    void selectUserByCpf_user_dont_exist() throws Exception {
+        doThrow(UserNotFoundException.class).when(service).selectByCpf(USER1.getCpf());
+
+        mvc.perform(MockMvcRequestBuilders.get("/api/v1/user/cpf/" + USER1.getCpf())
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(MockMvcResultMatchers.status().isNotFound());
     }
 
     @Test
