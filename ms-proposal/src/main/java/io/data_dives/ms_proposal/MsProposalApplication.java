68e8b9cb7bba8d4cf9a88dbd07ec9c685875857b
gerson <ishgardstorm@gmail.com>
2024-07-07 12:57:23 -0300

    feat: created the ms-proposal project structure

---
+++ ms-proposal/src/main/java/io/data_dives/ms_proposal/MsProposalApplication.java
@@ -0,0 +1,13 @@
+package io.data_dives.ms_proposal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsProposalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsProposalApplication.class, args);
+	}
+
+}
