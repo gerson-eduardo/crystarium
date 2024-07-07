4bffc35531e665627d96ea392de464ae543d16d7
gerson <ishgardstorm@gmail.com>
2024-07-07 17:13:17 -0300

    feat: Added Jpa Annotations and created a new constructor to the Proposal entity class

--- ms-proposal/src/main/java/io/data_dives/ms_proposal/model/Proposal.java
+++ ms-proposal/src/main/java/io/data_dives/ms_proposal/model/Proposal.java
@@ -1,5 +1,7 @@
 package io.data_dives.ms_proposal.model;
 
+import io.data_dives.ms_proposal.dto.CreateProposalDto;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -9,12 +11,27 @@ import java.time.ZonedDateTime;
 @AllArgsConstructor
 @NoArgsConstructor
 @Data
+@Entity
+@Table(name = "proposals")
 public class Proposal {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    @Column(nullable = false, length = 32)
     private String title;
+    @Column(columnDefinition = "TEXT")
     private String description;
-    private boolean appoved;
+    private boolean approved;
+    @Column(length = 11)
     private String cpf;
+    @Column(name = "created_at", nullable = false)
     private ZonedDateTime createdAt;
+    @Column(name = "modified_at", nullable = false)
     private ZonedDateTime modifiedAt;
+
+    public Proposal(CreateProposalDto dto){
+        this.title = dto.getTitle();
+        this.description = dto.getDescription();
+        this.cpf = dto.getCpf();
+    }
 }
