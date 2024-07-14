8bf558aa2fed540ad592e7beb1ed72378b68ed85
gerson <ishgardstorm@gmail.com>
2024-07-14 13:03:54 -0300

    feat: created a new constructor to the Vote entity class

--- ms-proposal/src/main/java/io/data_dives/ms_proposal/model/Vote.java
+++ ms-proposal/src/main/java/io/data_dives/ms_proposal/model/Vote.java
@@ -1,5 +1,6 @@
 package io.data_dives.ms_proposal.model;
 
+import io.data_dives.ms_proposal.dto.CreateVoteDto;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -13,7 +14,17 @@ public class Vote {
     private Long id;
     private String cpf;
     private Proposal proposal;
-    private Boolean approved;
+    private boolean approved;
     private ZonedDateTime createdAt;
     private ZonedDateTime modifiedAt;
+
+    public Vote(CreateVoteDto dto){
+        this.cpf = dto.getCpf();
+        this.approved = dto.getApproved();
+    }
+
+    public Vote removeId(){
+        setId(null);
+        return this;
+    }
 }
