43973381dc9656f17858b62821d600f45a4ad7e6
gerson <ishgardstorm@gmail.com>
2024-07-16 18:02:37 -0300

    feat: Updated the Proposal controler to require an IProposalService implementation

--- ms-proposal/src/main/java/io/data_dives/ms_proposal/controller/v1/ProposalController.java
+++ ms-proposal/src/main/java/io/data_dives/ms_proposal/controller/v1/ProposalController.java
@@ -1,21 +1,18 @@
 package io.data_dives.ms_proposal.controller.v1;
 
 import io.data_dives.ms_proposal.dto.CreateProposalDto;
-import io.data_dives.ms_proposal.model.Proposal;
-import io.data_dives.ms_proposal.service.v1.ProposalService;
+import io.data_dives.ms_proposal.service.IProposalService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
-
-import java.util.List;
 
 @RequestMapping("/api/v1")
 @RestController
 public class ProposalController {
 
     @Autowired
-    private ProposalService service;
+    private IProposalService service;
 
     @PostMapping("/proposal")
     public ResponseEntity<String> createProposal(@RequestBody CreateProposalDto dto){
