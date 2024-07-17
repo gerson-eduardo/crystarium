94d9aa3f19fe47b6405ae605ef2bf6e3599bb178
gerson <ishgardstorm@gmail.com>
2024-07-17 14:47:39 -0300

    test: created tests for the createPool service method

--- ms-proposal/src/test/java/io/data_dives/ms_proposal/service/v1/PoolServiceTest.java
+++ ms-proposal/src/test/java/io/data_dives/ms_proposal/service/v1/PoolServiceTest.java
@@ -1,13 +1,77 @@
 package io.data_dives.ms_proposal.service.v1;
 
+import io.data_dives.ms_proposal.ex.PoolAlreadyStartedException;
+import io.data_dives.ms_proposal.ex.ProposalNotFoundException;
+import io.data_dives.ms_proposal.model.Pool;
+import io.data_dives.ms_proposal.repository.PoolRepository;
+import io.data_dives.ms_proposal.repository.ProposalRepository;
+import io.data_dives.ms_proposal.repository.VoteRepository;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.time.Clock;
+import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import static io.data_dives.ms_proposal.props.PoolProps.*;
+import static org.mockito.Mockito.*;
+
+@ExtendWith(MockitoExtension.class)
 class PoolServiceTest {
+    @Mock
+    private PoolRepository poolRepository;
+    @Mock
+    private ProposalRepository proposalRepository;
+    @Mock
+    private VoteRepository voteRepository;
+    @Mock
+    private Clock clock;
+    @InjectMocks
+    private PoolService service;
 
     @Test
-    void createPool() {
+    void createPool_pool_is_valid() {
+        when(clock.instant()).thenReturn(NOW.toInstant());
+        when(clock.getZone()).thenReturn(NOW.getZone());
+        when(poolRepository.findByProposal_Id(1L)).thenReturn(Optional.empty());
+        when(proposalRepository.findById(1L)).thenReturn(Optional.of(POOL1.getProposal()));
+
+        assertDoesNotThrow(() -> service.createPool(1L));
+
+        verify(poolRepository, atLeast(1)).findByProposal_Id(1L);
+        verify(proposalRepository, atLeast(1)).findById(1L);
+        verify(poolRepository, atLeast(1)).save(any(Pool.class));
+    }
+
+    @Test
+    void createPool_pool_already_started() {
+        when(clock.instant()).thenReturn(NOW.toInstant());
+        when(clock.getZone()).thenReturn(NOW.getZone());
+        when(poolRepository.findByProposal_Id(1L)).thenReturn(Optional.of(POOL1));
+
+        assertThrows(PoolAlreadyStartedException.class, () -> service.createPool(1L));
+
+        verify(poolRepository, atLeast(1)).findByProposal_Id(1L);
+        verify(proposalRepository, never()).findById(1L);
+        verify(poolRepository, never()).save(any(Pool.class));
+    }
+
+    @Test
+    void createPool_proposal_not_found() {
+        when(clock.instant()).thenReturn(NOW.toInstant());
+        when(clock.getZone()).thenReturn(NOW.getZone());
+        when(poolRepository.findByProposal_Id(1L)).thenReturn(Optional.empty());
+        when(proposalRepository.findById(1L)).thenReturn(Optional.empty());
+
+        assertThrows(ProposalNotFoundException.class, () -> service.createPool(1L));
+
+        verify(poolRepository, atLeast(1)).findByProposal_Id(1L);
+        verify(proposalRepository, atLeast(1)).findById(1L);
+        verify(poolRepository, never()).save(any(Pool.class));
     }
 
     @Test
