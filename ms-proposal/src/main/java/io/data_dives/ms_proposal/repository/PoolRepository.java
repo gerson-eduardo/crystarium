b38e1c9efcb6980328ec39fb4d30b297057f5473
gerson <ishgardstorm@gmail.com>
2024-07-17 13:21:59 -0300

    feat: created the Pool interface methods endPool and findByProposal_Id

--- ms-proposal/src/main/java/io/data_dives/ms_proposal/repository/PoolRepository.java
+++ ms-proposal/src/main/java/io/data_dives/ms_proposal/repository/PoolRepository.java
@@ -3,6 +3,9 @@ package io.data_dives.ms_proposal.repository;
 import io.data_dives.ms_proposal.model.Pool;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface PoolRepository extends JpaRepository<Pool, Long> {
 
+    Optional<Pool> findByProposal_Id(Long id);
 }
--- ms-proposal/src/main/java/io/data_dives/ms_proposal/service/IPoolService.java
+++ ms-proposal/src/main/java/io/data_dives/ms_proposal/service/IPoolService.java
@@ -2,4 +2,5 @@ package io.data_dives.ms_proposal.service;
 
 public interface IPoolService {
     public void createPool(Long id);
+    public void endPool(Long id);
 }
