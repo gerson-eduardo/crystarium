cf81e3313c2a5056bdb533a48dc1a02d220c5845
gerson <ishgardstorm@gmail.com>
2024-07-17 13:32:43 -0300

    test: created the PoolControllerTest and PoolServiceTest classes

---
+++ ms-proposal/src/test/java/io/data_dives/ms_proposal/controller/v1/PoolControllerTest.java
@@ -0,0 +1,16 @@
+package io.data_dives.ms_proposal.controller.v1;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PoolControllerTest {
+
+    @Test
+    void startPool() {
+    }
+
+    @Test
+    void endPool() {
+    }
+}
---
+++ ms-proposal/src/test/java/io/data_dives/ms_proposal/service/v1/PoolServiceTest.java
@@ -0,0 +1,16 @@
+package io.data_dives.ms_proposal.service.v1;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PoolServiceTest {
+
+    @Test
+    void createPool() {
+    }
+
+    @Test
+    void endPool() {
+    }
+}
