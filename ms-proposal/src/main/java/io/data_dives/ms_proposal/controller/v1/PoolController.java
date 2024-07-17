414a5b739ab0c37eea0ea6529af284be707e722b
gerson <ishgardstorm@gmail.com>
2024-07-17 15:09:36 -0300

    feat: finished implementing the endPool method inside the PoolController

--- ms-proposal/src/main/java/io/data_dives/ms_proposal/controller/v1/PoolController.java
+++ ms-proposal/src/main/java/io/data_dives/ms_proposal/controller/v1/PoolController.java
@@ -1,5 +1,9 @@
 package io.data_dives.ms_proposal.controller.v1;
 
+import io.data_dives.ms_proposal.ex.PoolAlreadyStartedException;
+import io.data_dives.ms_proposal.ex.PoolNotEndedException;
+import io.data_dives.ms_proposal.ex.PoolNotFoundException;
+import io.data_dives.ms_proposal.ex.ProposalNotFoundException;
 import io.data_dives.ms_proposal.service.IPoolService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatusCode;
@@ -21,8 +25,24 @@ public class PoolController {
         try {
             service.createPool(id);
             return ResponseEntity.ok("Pool started");
+        }catch (ProposalNotFoundException e){
+            return new ResponseEntity<>(HttpStatusCode.valueOf(404));
+        }catch (PoolAlreadyStartedException e){
+            return new ResponseEntity<>(HttpStatusCode.valueOf(409));
         }catch (Exception e){
             return new ResponseEntity<>(HttpStatusCode.valueOf(500));
+        }
+    }
+
+    @PostMapping("/pool/end/{id}")
+    public ResponseEntity<String> endPool(@PathVariable Long id){
+        try {
+            service.endPool(id);
+            return ResponseEntity.ok("Pool ended sucessfully");
+        }catch (PoolNotFoundException e){
+            return new ResponseEntity<>(HttpStatusCode.valueOf(404));
+        }catch (PoolNotEndedException e){
+            return new ResponseEntity<>(HttpStatusCode.valueOf(409));
         }
     }
 }
